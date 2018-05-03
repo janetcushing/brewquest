7b00c81f82bd93c9247bb87421284f17605465d6
janetcushing <janet.cushing@gmail.com>
2018-05-03 14:32:10 -0400

    committing so i can repull from master

--- client/src/components/Mapmarker.js
+++ client/src/components/Mapmarker.js
@@ -1,45 +1,73 @@
-import React, { Component } from "react";
-import { Marker } from "react-google-maps"
-import { InfoWindow } from 'react-google-maps';
+import React, {
+    Component
+} from "react";
+import {
+    Marker
+} from "react-google-maps"
+import {
+    InfoWindow
+} from 'react-google-maps';
 
 class Mapmarker extends Component {
     constructor(props, context) {
         super(props, context);
 
         this.state = {
-            infoWindowOpen: true
+            infoWindowOpen: false
         };
 
 
     }
 
+    static defaultProps = {
+        infoWindowOpen: false
+    };
+
     onToggleOpen = (infoWindowOpen) => {
-        if (!this.infoWindowOpen)
-            this.setState({ infoWindowOpen: true });
+        if (!infoWindowOpen) {
+            this.setState({
+                infoWindowOpen: true
+            });
+        } else {
+            this.setState({
+                infoWindowOpen: false
+            });
+        }
     }
 
-    onToggleClose = (infoWindowOpen) => {
-        this.setState({ infoWindowOpen: false });
-    }
+    // onToggleClose = (infoWindowOpen) => {
+    //     this.setState({
+    //         infoWindowOpen: false
+    //     });
+    // }
 
     render() {
 
-        return (
-            <div>
-                <Marker
-                    key={this.props.detailkey}
-                    icon={'http://maps.google.com/mapfiles/ms/icons/red-dot.png'}
-                    position={this.props.position}
-                // onClick={this.onToggleOpen(this.infoWindowOpen)}
-                >
+        return (<div>
+            <Marker key={
+                this.props.detailkey
+            }
+                icon={
+                    'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+                }
+                position={
+                    this.props.position
+                }
+                onClick={this.onToggleOpen(this.infoWindowOpen)}
+            >
 
-                    {this.infoWindowOpen && <InfoWindow
-                        onCloseClick={this.onToggleClose} >
-                        <h5>Info Window</h5>>
-                        </InfoWindow>
-                    }
-                </Marker>
-            </div >
+                {
+                    (this.infoWindowOpen) ?
+                        < InfoWindow
+                            onCloseClick={
+                                this.onToggleClose
+                            } >
+                            <h5 > Info Window </h5>
+                            >
+                </InfoWindow>
+                        : < InfoWindow />}
+                } </Marker>
+        </div >
         )
     }
 }
